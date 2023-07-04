60cc5c54c61d3b8f4089a1d90d96f6b1e0e64953
anniinakinnunen <anniina.he.kinnunen@gmail.com>
2023-07-04 18:40:46 +0300

    Exercise 4.10

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -7,20 +7,16 @@ const Blog = require('../models/blog')
 
 const initialBlogs = [
   {
-    _id: "5a422a851b54a676234d17f7",
     title: "React patterns",
     author: "Michael Chan",
     url: "https://reactpatterns.com/",
-    likes: 7,
-    __v: 0
+    likes: 7
   },
   {
-    _id: "5a422aa71b54a676234d17f8",
     title: "Go To Statement Considered Harmful",
     author: "Edsger W. Dijkstra",
     url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5,
-    __v: 0
+    likes: 5
   }
 ]
 
@@ -44,6 +40,27 @@ test('all blogs are returned', async () => {
 test('blog id-field has correct name', async () => {
   const response = await api.get('/api/blogs')
   expect(response.body[0].id).toBeDefined()
+})
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+  expect(response.body).toHaveLength(initialBlogs.length + 1)
+
+  const titles = response.body.map(r => r.title)
+  expect(titles).toContain('Canonical string reduction')
 })
 
 afterAll(async () => {
